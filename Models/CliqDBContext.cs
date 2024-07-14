92c25239b5cc360baa8cd5414a678a42307ac31e
shubh11me <todkarshubham11@gmail.com>
2024-07-14 14:54:58 +0530

    Add project files.

---
+++ Models/CliqDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cliqlearn.Models
+{
+    public class CliqDBContext:DbContext
+    {
+        public CliqDBContext(DbContextOptions<CliqDBContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cliqlearn.Models
+{
+    public class Student
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
